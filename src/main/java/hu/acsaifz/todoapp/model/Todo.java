37a7399c419d1e66af7888d5e75519f610c211eb
acsaifz <angeluszmail@gmail.com>
2022-12-26 19:49:24 +0100

    Todo model

---
+++ src/main/java/hu/acsaifz/todoapp/model/Todo.java
@@ -0,0 +1,31 @@
+package hu.acsaifz.todoapp.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@Entity
+public class Todo {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    private String description;
+    private LocalDateTime dateOfCreation;
+    private boolean completed;
+
+    public Todo(String description){
+        this.description = description;
+        this.dateOfCreation = LocalDateTime.now();
+    }
+}
